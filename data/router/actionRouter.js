a9b0b45baafc1fa0fb396fb7b52e49d85dbcef6b
Jacob McFaul <mcjakedude@gmail.com>
2019-03-29 09:51:48 -0700

    Delete functions working and self study questions finished - MVP

--- data/router/actionRouter.js
+++ data/router/actionRouter.js
@@ -30,4 +30,20 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = await Action.remove(req.params.id);
+        if(id > 0){
+            res.status(200).json({message: 'Action has been deleted'})
+        }else{
+            res.status(404).json({message: 'Action not found'})
+        }   
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          message: 'Error removing action',
+        });
+    }
+});
+
 module.exports = router;
--- data/router/projectRouter.js
+++ data/router/projectRouter.js
@@ -16,6 +16,23 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const project = await Project.get(req.params.id);
+
+        if(project){
+            res.status(200).json(project);
+        }else{
+            res.status(404).json({message: 'Project not found'})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message:'Error getting project'
+        });
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
         const project = await Project.insert(req.body);
@@ -26,6 +43,22 @@ router.post('/', async (req, res) => {
         console.log(error)
         res.status(500).json({
             message:'Error posting project'
+        });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = await Project.remove(req.params.id);
+        if(id > 0){
+            res.status(200).json({message: 'Project has been deleted'})
+        }else{
+            res.status(404).json({message: 'Project not found'})
+        }   
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          message: 'Error removing project',
         });
     }
 });
